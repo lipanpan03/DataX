3175166d5fb951f7339d57de6ba5a47d0f7c38ed
Wang Zhiyong <botu.wzy@antgroup.com>
2024-04-30 19:33:01 +0800

    update tugraph writer

--- tugraphwriter/src/main/java/com/alibaba/datax/plugin/writer/tugraphwriter/Key.java
+++ tugraphwriter/src/main/java/com/alibaba/datax/plugin/writer/tugraphwriter/Key.java
@@ -1,14 +1,12 @@
 package com.alibaba.datax.plugin.writer.tugraphwriter;
 
 public final class Key {
-    public final static String HOST = "host";
-    public final static String PORT = "port";
+    public final static String URLS = "urls";
     public final static String USERNAME = "username";
     public final static String PASSWORD = "password";
     public final static String GRAPH_NAME = "graphName";
-    public final static String SCHEMA = "schema";
-    public final static String FILES = "files";
-    public final static String IGNORE_ERRORS = "ignoreErrors";
+    public final static String LABEL_NAME = "labelName";
+    public final static String LABEL_TYPE = "labelType";
+    public final static String PROPERTIES = "properties";
     public final static String BATCH_NUM = "batchNum";
-    public final static String BATCH_SIZE = "batchSize";
 }
--- tugraphwriter/src/main/java/com/alibaba/datax/plugin/writer/tugraphwriter/TuGraphWriter.java
+++ tugraphwriter/src/main/java/com/alibaba/datax/plugin/writer/tugraphwriter/TuGraphWriter.java
@@ -6,20 +6,11 @@ import com.alibaba.datax.common.exception.DataXException;
 import com.alibaba.datax.common.plugin.RecordReceiver;
 import com.alibaba.datax.common.spi.Writer;
 import com.alibaba.datax.common.util.Configuration;
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import org.apache.http.Consts;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpRequestBase;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
+import com.antgroup.tugraph.TuGraphDbRpcClient;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,233 +20,168 @@ import java.util.List;
 import java.util.Map;
 
 public class TuGraphWriter extends Writer {
-    private static int HTTP_TIMEOUT_INMILLIONSECONDS = 600000;
-    private static final int POOL_SIZE = 10;
-    private static final String NEWLINE_FLAG = System.getProperty("line.separator", "\n");
-
-    private static String executeAndGet(CloseableHttpClient httpClient, HttpRequestBase httpRequestBase) throws Exception {
-        HttpResponse response;
-        String entiStr = "";
-        response = httpClient.execute(httpRequestBase);
-        HttpEntity entity = response.getEntity();
-        if (entity != null) {
-            entiStr = EntityUtils.toString(entity, Consts.UTF_8);
-        }
-        if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
-            System.err.println("请求地址：" + httpRequestBase.getURI() + ", 请求方法：" + httpRequestBase.getMethod()
-                    + ",STATUS CODE = " + response.getStatusLine().getStatusCode());
-            httpRequestBase.abort();
-            throw new Exception("Response Status Code : " + response.getStatusLine().getStatusCode()
-                    + ", content : "  + entiStr);
-        }
-        return entiStr;
-    }
-
     public static class Job extends Writer.Job {
         private static final Logger LOG = LoggerFactory
                 .getLogger(Job.class);
-        private Configuration originalConfig;
+        private Configuration config;
 
         @Override
         public void init() {
-            this.originalConfig = super.getPluginJobConf();
-            int batctNum = originalConfig.getInt(Key.BATCH_NUM, 0);
-            int batctSize = originalConfig.getInt(Key.BATCH_SIZE, 1024 * 1024 * 10);
-            if (batctNum <= 0 && batctSize <= 0) {
-                throw DataXException.asDataXException(TuGraphWriterErrorCode.PARAMETER_ERROR,
-                        "batctCount 或者 batctSize 设置错误，至少一个大于0");
-            }
-        }
-
-        @Override
-        public void prepare() {
-            Object schema = originalConfig.get(Key.SCHEMA);
-            if (schema == null) {
-                return;
-            }
-            String username = originalConfig.getString(Key.USERNAME);
-            String password = originalConfig.getString(Key.PASSWORD);
-            String graphname = originalConfig.getString(Key.GRAPH_NAME);
-
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("schema", schema);
-            String schemaDescription = JSON.toJSONString(jsonObject);
-            String httpBaseUrl = String.format("http://%s:%d", originalConfig.getString(Key.HOST), originalConfig.getInt(Key.PORT));
-            RequestConfig requestConfig = RequestConfig.custom().setSocketTimeout(HTTP_TIMEOUT_INMILLIONSECONDS)
-                    .setConnectTimeout(HTTP_TIMEOUT_INMILLIONSECONDS).setConnectionRequestTimeout(HTTP_TIMEOUT_INMILLIONSECONDS)
-                    .setStaleConnectionCheckEnabled(true).build();
-            CloseableHttpClient httpClient = HttpClientBuilder.create().setMaxConnTotal(POOL_SIZE).setMaxConnPerRoute(POOL_SIZE)
-                    .setDefaultRequestConfig(requestConfig).build();
-
-            String token;
-            // login
-            {
-                Map<String, String> body = new HashMap<>();
-                body.put("user", username);
-                body.put("password", password);
-                HttpPost httpPost = new HttpPost(httpBaseUrl + "/login");
-                httpPost.setHeader("Accept", "application/json");
-                httpPost.setHeader("Content-Type", "application/json");
-                httpPost.setEntity(new StringEntity(JSON.toJSONString(body), "UTF-8"));
-                try {
-                    String response = executeAndGet(httpClient, httpPost);
-                    token = "Bearer " + JSONObject.parseObject(response).getString("jwt");
-                } catch (Exception e) {
-                    throw DataXException.asDataXException(TuGraphWriterErrorCode.LOGIN_ERROR, "login失败:", e);
-                }
-            }
-            // import schema
-            {
-                Map<String, String> body = new HashMap<>();
-                body.put("description", schemaDescription);
-                HttpPost httpPost = new HttpPost(httpBaseUrl + "/db/" + graphname + "/schema/text");
-                httpPost.setHeader("Accept", "application/json");
-                httpPost.setHeader("Content-Type", "application/json");
-                httpPost.setHeader("Authorization",token);
-                httpPost.setEntity(new StringEntity(JSON.toJSONString(body), "UTF-8"));
-                try {
-                    String response = executeAndGet(httpClient, httpPost);
-                    String log = (String)JSONObject.parseObject(response).getOrDefault("log","");
-                    LOG.info("{} return log : {}",httpPost.getMethod(), log);
-                } catch (Exception e) {
-                    throw DataXException.asDataXException(TuGraphWriterErrorCode.IMPORT_SCHEMA_ERROR, "导入schema失败:", e);
-                }
-            }
+            this.config = super.getPluginJobConf();
         }
 
         @Override
         public void destroy() {
-
+            LOG.info("TuGraphWriter Job destroyed");
         }
 
         @Override
         public List<Configuration> split(int mandatoryNumber) {
             List<Configuration> writerSplitConfigs = new ArrayList<Configuration>();
             for (int i = 0; i < mandatoryNumber; i++) {
-                writerSplitConfigs.add(this.originalConfig);
+                writerSplitConfigs.add(this.config);
             }
 
             return writerSplitConfigs;
         }
     }
+    static class Property {
+        private String name;
+        private String type;
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getType() {
+            return type;
+        }
+    }
     public static class Task extends Writer.Task {
         private static final Logger LOG = LoggerFactory
                 .getLogger(Job.class);
-        private Configuration writerSliceConfig;
-        private String importDescription;
-        private String httpBaseUrl;
+        private Configuration config;
         private String username;
         private String password;
         private String graphname;
-        private String token;
-        boolean ignore_errors;
-        private int batctNum;
-        private int batctSize;
-        private CloseableHttpClient httpClient;
+        private String labelName;
+        private String labelType;
+        private int batchNum;
+        private List<Property> properties = new ArrayList<>();
+        private List<String> urls = new ArrayList<>();
+
+        TuGraphDbRpcClient client;
 
         @Override
         public void init() {
-            writerSliceConfig = super.getPluginJobConf();
-            username = writerSliceConfig.getString(Key.USERNAME);
-            password = writerSliceConfig.getString(Key.PASSWORD);
-            graphname = writerSliceConfig.getString(Key.GRAPH_NAME);
-            ignore_errors = writerSliceConfig.getBool(Key.IGNORE_ERRORS, false);
-            batctNum = writerSliceConfig.getInt(Key.BATCH_NUM, -1);
-            batctSize = writerSliceConfig.getInt(Key.BATCH_SIZE, 1024 * 1024 * 10);
-
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("files",writerSliceConfig.get(Key.FILES));
-            importDescription = JSON.toJSONString(jsonObject);
-
-            httpBaseUrl = String.format("http://%s:%d", writerSliceConfig.getString(Key.HOST), writerSliceConfig.getInt(Key.PORT));
-            RequestConfig requestConfig = RequestConfig.custom().setSocketTimeout(HTTP_TIMEOUT_INMILLIONSECONDS)
-                    .setConnectTimeout(HTTP_TIMEOUT_INMILLIONSECONDS).setConnectionRequestTimeout(HTTP_TIMEOUT_INMILLIONSECONDS)
-                    .setStaleConnectionCheckEnabled(true).build();
-            httpClient = HttpClientBuilder.create().setMaxConnTotal(POOL_SIZE).setMaxConnPerRoute(POOL_SIZE)
-                    .setDefaultRequestConfig(requestConfig).build();
-        }
-
-        @Override
-        public void prepare() {
-            // login
-            {
-                Map<String, String> body = new HashMap<>();
-                body.put("user", username);
-                body.put("password", password);
-                HttpPost httpPost = new HttpPost(httpBaseUrl + "/login");
-                httpPost.setHeader("Accept", "application/json");
-                httpPost.setHeader("Content-Type", "application/json");
-                httpPost.setEntity(new StringEntity(JSON.toJSONString(body), "UTF-8"));
-                try {
-                    String response = executeAndGet(httpClient, httpPost);
-                    JSONObject jsonObject = JSONObject.parseObject(response);
-                    token = "Bearer " + jsonObject.getString("jwt");
-                } catch (Exception e) {
-                    throw DataXException.asDataXException(TuGraphWriterErrorCode.LOGIN_ERROR, "login失败:", e);
+            config = super.getPluginJobConf();
+            username = config.getString(Key.USERNAME);
+            password = config.getString(Key.PASSWORD);
+            graphname = config.getString(Key.GRAPH_NAME);
+            labelType = config.getString(Key.LABEL_TYPE);
+            labelName = config.getString(Key.LABEL_NAME);
+            urls = config.getList(Key.URLS, String.class);
+            batchNum = config.getInt(Key.BATCH_NUM, 1000);
+            if (batchNum <= 0) {
+                throw new DataXException(TuGraphWriterErrorCode.PARAMETER_ERROR, "batchNum error");
+            }
+            String schema;
+            try {
+                if (urls.size() == 1) {
+                    client = new TuGraphDbRpcClient(urls.get(0), "admin", "73@TuGraph");
+                } else if (urls.size() > 1) {
+                    client = new TuGraphDbRpcClient(urls, "admin", "73@TuGraph");
+                } else {
+                    throw new DataXException(TuGraphWriterErrorCode.PARAMETER_ERROR, "urls size error");
                 }
+                if (labelType.equals("VERTEX")) {
+                    schema = client.callCypherToLeader(String.format("CALL db.getVertexSchema('%s')", labelName), graphname, 10000);
+                } else {
+                    schema = client.callCypherToLeader(String.format("CALL db.getEdgeSchema('%s')", labelName), graphname, 10000);
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            Gson gson = new Gson();
+            JsonArray jsonArray = gson.fromJson(schema, JsonArray.class);
+            JsonObject jsonObject = jsonArray.get(0).getAsJsonObject().get("schema").getAsJsonObject();
+            JsonArray array = jsonObject.get("properties").getAsJsonArray();
+            Map<String, String> allProperties = new HashMap<>();
+            for (JsonElement element : array) {
+                JsonObject obj = element.getAsJsonObject();
+                String name = obj.get("name").getAsString();
+                String type = obj.get("type").getAsString();
+                allProperties.put(name, type);
+            }
+            for (String name : config.getList(Key.PROPERTIES, String.class)) {
+                Property item = new Property();
+                item.setName(name);
+                item.setType(allProperties.get(name));
+                properties.add(item);
             }
         }
 
         @Override
         public void destroy() {
-
+            LOG.info("tugraph writer task destroyed.");
         }
 
-        private String recordToJsonArrayString(Record record) {
-            int recordLength = record.getColumnNumber();
-            if (0 == recordLength) {
-                return NEWLINE_FLAG;
-            }
-            Column column;
-            StringBuilder sb = new StringBuilder();
-            JSONArray jsonArray = new JSONArray();
-            for (int i = 0; i < recordLength; i++) {
-                column = record.getColumn(i);
-                jsonArray.add(column.asString());
-            }
-            sb.append(jsonArray.toString());
-            sb.append(NEWLINE_FLAG);
-            return sb.toString();
-        }
-
-        private void tugraphImportText(String data) {
-            Map<String, Object> body = new HashMap<>();
-            body.put("description", importDescription);
-            body.put("data", data);
-            body.put("continue_on_error", ignore_errors ? true : false);
-            HttpPost httpPost = new HttpPost(httpBaseUrl + "/db/" + graphname + "/import/text");
-            httpPost.setHeader("Accept", "application/json");
-            httpPost.setHeader("Content-Type", "application/json");
-            httpPost.setHeader("Authorization",token);
-            httpPost.setEntity(new StringEntity(JSON.toJSONString(body), "UTF-8"));
-            try {
-                String response = executeAndGet(httpClient, httpPost);
-                JSONObject jsonObject = JSONObject.parseObject(response);
-                String log = (String)jsonObject.getOrDefault("log","");
-                //LOG.info("{} return log : {}",httpPost.getMethod(), log);
-            } catch (Exception e) {
-                throw DataXException.asDataXException(TuGraphWriterErrorCode.IMPORT_DATA_ERROR, "导入数据失败:", e);
+        private void writeTugraph(JsonArray array) throws Exception {
+            String str = array.toString().replace("'", "''");
+            if (labelType.equals("VERTEX")) {
+                client.callCypherToLeader(String.format("CALL upsertVertexByJson('%s','%s')", labelName, str), graphname, 10000);
+            } else {
+                client.callCypherToLeader(String.format("CALL upsertEdgeByJson('%s','%s')", labelName, str), graphname, 10000);
             }
         }
 
         @Override
         public void startWrite(RecordReceiver recordReceiver) {
             try {
-                long count = 0;
                 StringBuilder builder = new StringBuilder();
                 Record record;
+                JsonArray array = new JsonArray();
                 while ((record = recordReceiver.getFromReader()) != null) {
-                    count++;
-                    builder.append(recordToJsonArrayString(record));
-                    if ((batctSize > 0 && builder.length() >= batctSize) ||
-                            (batctNum > 0 && count >= batctNum)) {
-                        tugraphImportText(builder.toString());
-                        builder = new StringBuilder();
-                        count = 0;
+                    int sourceColNum = record.getColumnNumber();
+                    if (sourceColNum != properties.size()) {
+                        throw new DataXException(TuGraphWriterErrorCode.PARAMETER_ERROR, "reader and writer columns size does not match!");
+                    }
+                    JsonObject obj = new JsonObject();
+                    for (int i = 0; i < sourceColNum; i++) {
+                        Column column = record.getColumn(i);
+                        Property pro = properties.get(i);
+                        if (pro.type.equals("BOOL")) {
+                            obj.addProperty(pro.name, column.asBoolean());
+                        } else if (pro.type.startsWith("INT")) {
+                            obj.addProperty(pro.name, column.asLong());
+                        } else if (pro.type.equals("DOUBLE")) {
+                            obj.addProperty(pro.name, column.asDouble());
+                        } else if (pro.type.equals("FLOAT")) {
+                            obj.addProperty(pro.name, column.asDouble());
+                        } else if (pro.type.equals("STRING")) {
+                            obj.addProperty(pro.name, column.asString());
+                        } else if (pro.type.startsWith("DATE")) {
+                            obj.addProperty(pro.name, column.asString());
+                        } else {
+                            throw new DataXException(TuGraphWriterErrorCode.PARAMETER_ERROR, "Unsupported data type " + pro.type);
+                        }
+                    }
+                    array.add(obj);
+                    if (array.size() >= batchNum) {
+                        writeTugraph(array);
+                        array = new JsonArray();
                     }
                 }
-                if (builder.length() > 0) {
-                    tugraphImportText(builder.toString());
+                if (array.size() > 0) {
+                    writeTugraph(array);
                 }
             } catch (Exception e) {
                     throw DataXException.asDataXException(TuGraphWriterErrorCode.RUNTIME_EXCEPTION, e);
